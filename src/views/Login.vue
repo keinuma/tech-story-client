208c017d10da5772981f2b00091ba966b12635db
keinuma <nununu.mono@gmail.com>
2020-09-27 23:52:12 +0900

    feat: use firebase authentication for login and register account

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,13 +1,52 @@
 <template>
   <h1>Login</h1>
+  <form class="form" @submit.prevent="login">
+    <label class="form-item">
+      メールアドレス
+      <input type="email" v-model="form.email" />
+    </label>
+    <label class="form-item">
+      パスワード
+      <input autocomplete type="password" v-model="form.password" />
+    </label>
+    <button class="form-item confirm-button" @click="login">確定</button>
+  </form>
 </template>
 
 <script lang="ts">
+import { reactive } from 'vue';
+import { auth } from '../main';
+
 export default {
-  name: "Login"
+  name: "Login",
+  setup() {
+    const form = reactive({
+      email: '',
+      password: ''
+    })
+    const login = async () => {
+      const res = await auth?.signInWithEmailAndPassword(form.email, form.password)
+      const token = await res?.user?.getIdToken()
+      console.log(token)
+    }
+    return {
+      form,
+      login
+    }
+  }
 }
 </script>
 
 <style scoped>
-
+.form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.form-item {
+  margin-top: 24px;
+}
+.confirm-button {
+  width: 300px;
+}
 </style>
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,13 +1,56 @@
 <template>
   <h1>Register</h1>
+  <form class="form" @submit.prevent="register">
+    <label class="form-item">
+      メールアドレス
+      <input type="email" v-model="form.email" />
+    </label>
+    <label class="form-item">
+      パスワード
+      <input autocomplete type="password" v-model="form.password" />
+    </label>
+    <button class="form-item confirm-button" @click="register">
+      確定
+    </button>
+  </form>
 </template>
 
 <script lang="ts">
+import { reactive } from 'vue'
+import { auth } from '../main'
+
 export default {
-  name: "Register"
+  name: "Register",
+  setup() {
+    const form = reactive({
+      email: '',
+      password: ''
+    })
+    const register = async () => {
+      const res = await auth?.createUserWithEmailAndPassword(form.email, form.password)
+      await res?.user?.sendEmailVerification({
+        url: window.location.toString(),
+        handleCodeInApp: false
+      })
+    }
+    return {
+      form,
+      register
+    }
+  }
 }
 </script>
 
 <style scoped>
-
+.form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.form-item {
+  margin-top: 24px;
+}
+.confirm-button {
+  width: 300px;
+}
 </style>
